247cba764773ec5611e481dbc9e0b37434756d97
Max Lincoln <mlincoln@glance.net>
2021-05-01 01:38:10 -0400

    e

--- run.py
+++ run.py
@@ -375,18 +375,18 @@ class Replica:
         print("IN DELIVER")
         if self.alive:
             try:
-                print("IN TRY")
-                print(self.rid)
-                print(self.client_sock.proto)
+                # print("IN TRY")
+                # print(self.rid)
+                # print(self.client_sock.proto)
                 writeable = []
                 while self.client_sock not in writeable:
                     ready, writeable, exception = select.select([self.client_sock], [self.client_sock], [], 0.1)
                 if self.client_sock in writeable:
                     self.client_sock.send(raw_msg)
-                print("AFTER TRY")
+                # print("AFTER TRY")
                 return True
             except:
-                print("IN EXCEPT")
+                # print("IN EXCEPT")
                 print '*** Simulator Error - Unable to send to replica'
                 self.shutdown()
         return False
